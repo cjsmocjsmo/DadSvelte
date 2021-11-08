a95eb917821f0214623300ac4907c50aff421e94
Charlie J Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2021-11-08 10:16:03 -0800

    added back button

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -16,7 +16,7 @@
 <svelte:head>
 	<title>Dad</title>
 </svelte:head>
-
+<a href="https://frontpage-czi.pages.dev/" ><button>Back</button></a>
 <div class="column">
 	<a href="/d1zoom"><img src={d1thumb} alt="fuckit" ></a>
 	<a href="/d2zoom"><img src={d2thumb} alt="fuckit" ></a>
@@ -28,8 +28,20 @@
 	<a href="/d9zoom"><img src={d9thumb} alt="fuckit" ></a>
 	<a href="/d10zoom"><img src={d10thumb} alt="fuckit" ></a>
 </div>
+<a href="https://frontpage-czi.pages.dev/" ><button>Back</button></a>
 
 <style>
+
+	button {
+        width: 25%;
+		padding: 8px;
+		border-radius: 7px;
+		background-color: white;
+		font-size: 1.25em;
+		color: black;
+    }
+
+
 	.column {
 		flex: 25%;
 		padding: 0 4px;
